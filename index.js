872f3bf93c18602d38a569487b8558e7566778d0
Alina M <alinaamoskalenko@gmail.com>
2023-01-05 19:36:23 -0800

    bootstrap added

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const path = require('path');
+const { engine } = require('express-handlebars');
 const logger = require('./middleware/logger');
+const members = require('./Members');
 
 const app = express();
 
 // Init middleware
 // app.use(logger);
 
+// Handlebars Middleware
+app.engine('handlebars', engine({ defaultlayout: 'main' }));
+app.set('view engine', 'handlebars');
+
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Homepage Route
+app.get('/', (req, res) =>
+  res.render('index', {
+    title: 'Member App',
+    members,
+  })
+);
 
 // Set a static folder (serves regular basic html files)
 app.use(express.static(path.join(__dirname, 'public')));
